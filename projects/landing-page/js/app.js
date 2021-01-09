f8701b99183c5213df0a8812e23492cd8d39ffbe
Debora Durek <debora.durek@gmail.com>
2021-01-08 20:23:11 -0600

    feat: Check section in viewport, add active class

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -55,34 +55,25 @@ const menuList = document.querySelector('#navbar__list'); // select ul to append
 allSections.forEach(function (section) {
   const menuItem = document.createElement('li'); // create li
   const menuItemLink = document.createElement('a'); // create link
-  menuItemLink.textContent = section.dataset.nav; // link text equal to section data-nav
-  menuItemLink.setAttribute('href', `#${section.id}`); // set href equal to id
+  menuItemLink.textContent = section.dataset.nav; // link text = section data-nav
+  menuItemLink.setAttribute('href', `#${section.id}`); // set href = section id
   menuItemLink.classList.add('menu__link'); // add class to link for styling
+  menuItemLink.classList.add(section.id); // add class to link = section id
   menuItem.appendChild(menuItemLink); // append link to li
   menuList.appendChild(menuItem); // append li to ul
 });
 
-// Find all links and add a listener to each one:
-// ToDo
-
-const allLinks = document.querySelectorAll('a');
-
-allLinks.forEach(function (eachLink) {
-  eachLink.addEventListener('click', function () {
-    eachLink.classList.add();
-    console.log(eachLink.classList);
-  });
-});
-
-// Detect if section is in viewport
+// Detect which section is in viewport and add active classes to Section and Menu Item
 
 function sectionActive() {
   for (const section of allSections) {
     const box = section.getBoundingClientRect();
     if (box.top <= 150 && box.bottom >= 150) {
       section.classList.add('active-class');
+      document.querySelector(`.${section.id}`).classList.add('active-link');
     } else {
       section.classList.remove('active-class');
+      document.querySelector(`.${section.id}`).classList.remove('active-link');
     }
   }
 }
