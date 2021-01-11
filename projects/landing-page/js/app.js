310e243c9e96138d49e9ed653700695d7f7784e8
Debora Durek <debora.durek@gmail.com>
2021-01-11 17:41:58 -0600

    refactor: Organise JS file

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,52 +1,7 @@
 /**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
+ * Global Variables
  *
  */
-
-/**
- * Define Global Variables
- *
- */
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
-// build the nav
-
-// Add class 'active' to section when near top of viewport
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
 
 // Transform all sections in an array
 const allSections = Array.from(document.getElementsByTagName('section'));
@@ -54,21 +9,31 @@ const allSections = Array.from(document.getElementsByTagName('section'));
 // Select ul to append li
 const menuList = document.querySelector('#navbar__list');
 
+// Set time to hide navBar when no action is detected
+let time = null;
+
+/**
+ * End Global Variables
+ * Begin Main Functions
+ *
+ */
+
 // Build dynamic navbar and append all menu items
-allSections.forEach(function (section) {
-  const menuItem = document.createElement('li');
-  const menuItemLink = document.createElement('a');
-  menuItemLink.textContent = section.dataset.nav;
-  menuItemLink.setAttribute('href', `#${section.id}`);
-  menuItemLink.classList.add('menu__link');
-  menuItemLink.classList.add(section.id);
-  menuItemLink.addEventListener('click', scrollToView(section.id));
-  menuItem.appendChild(menuItemLink);
-  menuList.appendChild(menuItem);
-});
+function buildNavBar() {
+  allSections.forEach(function (section) {
+    const menuItem = document.createElement('li');
+    const menuItemLink = document.createElement('a');
+    menuItemLink.textContent = section.dataset.nav;
+    menuItemLink.setAttribute('href', `#${section.id}`);
+    menuItemLink.classList.add('menu__link');
+    menuItemLink.classList.add(section.id);
+    menuItemLink.addEventListener('click', scrollToView(section.id));
+    menuItem.appendChild(menuItemLink);
+    menuList.appendChild(menuItem);
+  });
+}
 
-// Detect which section is in viewport and add active classes to Section and Menu Item
-
+// Detect which section is in viewport and set as active
 function sectionActive() {
   for (const section of allSections) {
     const box = section.getBoundingClientRect();
@@ -82,16 +47,12 @@ function sectionActive() {
   }
 }
 
-document.addEventListener('scroll', sectionActive);
-
-// Scroll to anchor ID using scrollTO event
-
+// Scroll to section on link click
 function scrollToView(sectionId) {
   // Returns section id from sectionActive()
   return function (event) {
     const menuListHeight = document.getElementById('navbar__list').offsetHeight;
     const section = document.getElementById(sectionId);
-
     event.preventDefault();
     // Scroll smoothly to section considering navBar height
     window.scroll({
@@ -102,34 +63,9 @@ function scrollToView(sectionId) {
 }
 
 // Hide navBar when no action is detected
-
-// Set time
-let time = null;
-
-// Hides navBar
 function hideMenu() {
   menuList.style.display = 'none';
 }
-
-// NavBar hides after 2 seconds if no scroll is detected
-window.addEventListener('scroll', function () {
-  if (time !== null) {
-    menuList.style.display = 'block';
-    clearTimeout(time);
-  }
-  time = setTimeout(hideMenu, 2000);
-});
-
-// NavBar hides after 2 seconds if no mousemove is detected
-window.addEventListener('mousemove', function () {
-  if (time !== null) {
-    menuList.style.display = 'block';
-    clearTimeout(time);
-  }
-  time = setTimeout(hideMenu, 2000);
-});
-
-// Scroll to top using button which gets visible only below fold of the page
 
 // Create button to scroll to top
 function buttonToTop() {
@@ -150,21 +86,68 @@ function buttonToTop() {
 
 // Scroll to top after user clicks on the created button
 function goToTop() {
-  document.body.scrollTop = 0; // For Safari users
+  document.body.scrollTop = 0; // Safari users
   document.documentElement.scrollTop = 0;
 }
 
-buttonToTop();
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
 
+// Event: scroll event to make both section and link active
+document.addEventListener('scroll', sectionActive);
+
+/* Event for hiding NavBar Starts */
+// Event: scroll event to hide NavBar after 2 seconds if no scroll is detected
+window.addEventListener('scroll', function () {
+  if (time !== null) {
+    menuList.style.display = 'block';
+    clearTimeout(time);
+  }
+  time = setTimeout(hideMenu, 2000);
+});
+
+// Event: mousemove event to hide NavBar after 2 seconds if no mousemove is detected
+window.addEventListener('mousemove', function () {
+  if (time !== null) {
+    menuList.style.display = 'block';
+    clearTimeout(time);
+  }
+  time = setTimeout(hideMenu, 2000);
+});
+/* Event for hiding NavBar Ends */
+
+// Event: scroll event to go back to top
 document.addEventListener('scroll', function () {
   const [buttonUp] = document.getElementsByClassName('button-up');
   const offset = window.pageYOffset;
   const pageFold = window.innerHeight / 2;
-  console.log(offset);
-  console.log(pageFold);
+
   if (offset > pageFold) {
     buttonUp.style.display = 'flex';
   } else {
     buttonUp.style.display = 'none';
   }
 });
+
+// Event: load event to set page on top after refresh
+window.addEventListener('beforeunload', goToTop);
+
+/**
+ * End Events
+ * Begin Function Calls
+ *
+ */
+
+// Build navbar
+buildNavBar();
+
+// Scroll to top using button which gets visible only below fold of the page
+buttonToTop();
+
+/**
+ * End Events
+ *
+ */
